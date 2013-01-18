6de93454f7a0281b2715b9bb41eee217ecedb8e1
rsf2 <russ.frisch@me.com>
2013-01-18 16:54:29 -0500

    Finished Part 3 homework

--- hw1/part3.rb
+++ hw1/part3.rb
@@ -1,3 +1,15 @@
 def combine_anagrams(words)
- # YOUR CODE HERE
+  output = {}
+  words.each do |word|
+    if output[sort_word word].nil?
+      output[sort_word word] = [ word ]
+    else
+      output[sort_word word] << word
+    end
+  end
+  output.values
+end
+
+def sort_word(word)
+  word.downcase.chars.sort.join
 end
--- hw1/part3_test.rb
+++ hw1/part3_test.rb
@@ -1,3 +1,7 @@
-def combine_anagrams(words)
- # YOUR CODE HERE
-end
+require File.dirname(__FILE__) + '/part3'
+
+#puts combine_anagrams(['cars', 'for', 'potatoes', 'racs', 'four', 'scar', 'creams', 'scream']).to_s
+
+puts combine_anagrams(["HeLLo", "hello"]).to_s
+
+#puts combine_anagrams(['a', 'A']).to_s
